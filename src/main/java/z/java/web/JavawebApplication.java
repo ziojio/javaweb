243ef0d56b4c0787bb6599dcbe3659e67c04f34b
ziojio <ziojio@qq.com>
2022-07-15 10:25:16 +0800

    Initial JavaWeb

---
+++ src/main/java/z/java/web/JavawebApplication.java
@@ -0,0 +1,13 @@
+package z.java.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/z/java/web/JavawebApplicationTests.java
@@ -0,0 +1,13 @@
+package z.java.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
