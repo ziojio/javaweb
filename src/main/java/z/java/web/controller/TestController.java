6e714a4b3f5d832756a2478c88c2243b6f13977b
ziojio <ziojio@qq.com>
2022-07-17 19:45:39 +0800

    Web 解决/error问题，未处理http错误，例如404等会报这个错，而不是/error它本身未处理

---
+++ src/main/java/z/java/web/controller/TestController.java
@@ -0,0 +1,16 @@
+package z.java.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test data";
+    }
+
+}
